796d5a1c81353ef266cced4366340c090b3ab256
EdgarHuante <edgareduardohuantejeronimo@gmail.com>
2026-03-20 08:56:54 -0600

    ss

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -2,5 +2,5 @@ export { Toast } from './Toast';
 export { ToastContainer } from './ToastContainer';
 export { ErrorBoundary } from './ErrorBoundary';
 export { Spinner } from './Spinner';
-export { Icon, Icons } from './Icon';
+export { Icon } from './Icon';
 export type { IconName } from './Icon';
